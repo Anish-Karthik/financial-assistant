13a198af7df01d80bf86b86e5230b1213d17ea8e
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-11-21 20:57:29 +0530

    added axios

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const cheerio = require('cheerio');
 const countryStateCity = require('country-state-city');
 const cors = require('cors');
-const { json } = require('body-parser');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors());              
 
 app.get('/extract-values', async (req, res) => {
   // get search parameters
@@ -32,7 +32,7 @@ app.get('/extract-values', async (req, res) => {
       url += `${city}/`;
     }
     console.log("***************************************", country)
-    const response = await fetch(url)
+    const response = await axios.get(url);
     const html = await response.text();
     // console.log(html)
 
